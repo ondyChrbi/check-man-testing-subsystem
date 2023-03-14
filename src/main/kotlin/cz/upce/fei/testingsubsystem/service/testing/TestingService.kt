56074a449f5d6fe9f78cd973b27562154cf809db
ondyChrbi <ondrej.chrbolka@icloud.com>
2023-03-14 14:17:05 +0100

    Unzip files and fix small bugs

--- src/main/kotlin/cz/upce/fei/testingsubsystem/service/testing/TestingService.kt
+++ src/main/kotlin/cz/upce/fei/testingsubsystem/service/testing/TestingService.kt
@@ -4,10 +4,15 @@ import cz.upce.fei.testingsubsystem.domain.Solution
 import cz.upce.fei.testingsubsystem.domain.TestConfiguration
 import cz.upce.fei.testingsubsystem.repository.TestConfigurationRepository
 import jakarta.annotation.PostConstruct
+import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream
+import org.apache.commons.compress.utils.IOUtils
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
@@ -54,8 +59,53 @@ class TestingService(private val testConfigurationRepository: TestConfigurationR
         if (Files.notExists(templatePath)) { throw TemplateFileNotExistException(templatePath) }
         if (Files.notExists(dockerFilePath)) { throw DockerFileNotExistException(dockerFilePath) }
 
-        Files.copy(templatePath, testingPlaygroundDir.resolve(templatePath.fileName))
+        val templateZipLocation = Files.copy(templatePath, testingPlaygroundDir.resolve(templatePath.fileName))
+        val unzipTemplateLocation = unzipFile(templateZipLocation, testingPlaygroundDir.resolve(Paths.get(TEMPLATE_DIR_NAME)))
+        logger.debug("Zip $templateZipLocation unarchived to location $unzipTemplateLocation")
+
         Files.copy(dockerFilePath, testingPlaygroundDir.resolve(DOCKER_FILE_NAME))
+    }
+
+    private fun unzipFile(zipFilePath: Path, destDirPath: Path, deleteSource: Boolean = true): Path {
+        val zipFile = zipFilePath.toFile()
+        val destDir = destDirPath.toFile()
+
+        if (!zipFile.exists()) {
+            throw IllegalArgumentException("Zip file not found: $zipFilePath")
+        }
+
+        if (!destDir.exists()) {
+            Files.createDirectories(destDirPath)
+        }
+
+        val zipInputStream = ZipArchiveInputStream(FileInputStream(zipFile))
+        var entry = zipInputStream.nextEntry
+
+        while (entry != null) {
+            val entryFile = File(destDir, entry.name)
+
+            if (entry.isDirectory) {
+                Files.createDirectories(entryFile.toPath())
+            } else {
+                val entryDir = entryFile.parentFile
+                if (!entryDir.exists()) {
+                    Files.createDirectories(entryDir.toPath())
+                }
+                val entryOutputStream = FileOutputStream(entryFile)
+                IOUtils.copy(zipInputStream, entryOutputStream)
+                entryOutputStream.close()
+            }
+
+            entry = zipInputStream.nextEntry
+        }
+
+        zipInputStream.close()
+
+        if (deleteSource) {
+            Files.deleteIfExists(zipFilePath)
+        }
+
+        return destDirPath
     }
 
     private fun copySolutionToPlayground(solution: Solution, testingPlaygroundDir: Path): Path {
@@ -66,13 +116,18 @@ class TestingService(private val testConfigurationRepository: TestConfigurationR
         }
 
         val location = testingPlaygroundDir.resolve(solutionFile.fileName)
-        Files.copy(solutionFile, location)
+        val zipLocation = Files.copy(solutionFile, location)
         logger.debug("File  ${solution.id}: ${solution.path}")
+
+        val unzipLocation = unzipFile(zipLocation, testingPlaygroundDir.resolve(Paths.get(SOLUTION_DIR_NAME)))
+        logger.debug("Zip $zipLocation unarchived to location $unzipLocation")
 
         return location
     }
 
     private companion object {
-        const val DOCKER_FILE_NAME = "DockerFile"
+        const val DOCKER_FILE_NAME = "Dockerfile"
+        const val SOLUTION_DIR_NAME = "solution"
+        const val TEMPLATE_DIR_NAME = "template"
     }
 }
